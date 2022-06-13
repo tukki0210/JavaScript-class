71ac9ec721b29206f5ee10fbb4a9cb981a1824d7
tukki0210 <tukki0210@gmail.com>
2022-06-13 15:15:56 +0900

    作例

--- 6月/quiz.js
+++ 6月/quiz.js
@@ -17,54 +17,44 @@ const fuseikai = "×不正解"
 // 共通部分をまとめるのが関数
 // 関数名は動詞＋名詞でつけることが多い
 
-// 不正解だったときの関数
-// 引数idNameを設定して、A,B,Dを当てはめる
-const makeFuseikai = function (idName) {
-    document.getElementById(idName).addEventListener('click', function () {
-        this.textContent = fuseikai;
-        this.setAttribute('class', 'fuseikai');
+// 不正解の関数
+const makeFuseikai = (idName) => {
+
+    const fuseikaiEl = document.getElementById(idName);
+
+    fuseikaiEl.addEventListener('click', () => {
+        fuseikaiEl.textContent = fuseikai;
+        fuseikaiEl.setAttribute('class', 'fuseikai');
     })
 }
 
-// 正解のときの関数
+// 正解の関数
 const makeSeikai = function (idName) {
     document.getElementById(idName).addEventListener('click', function () {
         this.textContent = seikai;
-        this.setAttribute('class','seikai');
+        this.setAttribute('class', 'seikai');
     })
 }
 
 // 短くできるけど、分かりにくくなるかも
-const makeAnswer = function (idName, answer){
-    document.getElementById(idName).addEventListener('click', function () {
-        this.textContent = answer;
-        this.setAttribute('class',answer);
-    })
+const makeAnswer = (idName, answer) => {
+    const answerEl = document.getElementById(idName);
+
+    // 名前を付けて外に持ってくる
+    const answerChange = () => {
+        answerEl.textContent = answer;
+        answerEl.setAttribute('class', answer);
+    }
+
+    answerEl.addEventListener('click', answerChange)
 }
 
-// 関数の実行
-// makeFuseikai('answerA');
-// makeFuseikai('answerB');
-// makeSeikai('answerC');
-// makeFuseikai('answerD');
+makeAnswer('answerA', 'fuseikai');
+makeAnswer('answerB', 'fuseikai');
+makeAnswer('answerC', 'seikai');
+makeAnswer('answerD', 'fuseikai');
 
-makeAnswer('answerA','fuseikai');
-makeAnswer('answerB','fuseikai');
-makeAnswer('answerC','seikai');
-makeAnswer('answerD','fuseikai');
-
-// a 5秒が来たら動くプログラム
-setTimeout(function(){
-    console.log('5秒立ったよ！')
-},5000);
-
-// b 問題文のテキストを終了に変える
-// 指定した要素（element）.textContent で文字を変更できる
-// document.getElementById('mondai').textContent = '終了';
-
-// aとbを組み合わせる
-setTimeout(function(){
+setTimeout(function () {
     document.getElementById('mondai').textContent = '終了';
-},5000);
+}, 5000);
 
-document.querySelector('form').setAttribute('action','https://www.wana.com')
